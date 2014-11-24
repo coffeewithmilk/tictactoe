5dd09b0deea5689f6dc2b2012ef8d889cdd1e314
DavidSjogren <sdavid2304@gmail.com>
2014-11-24 23:49:24 +0100

    GREEN

--- tictactoe/src/tictactoe/Board.java
+++ tictactoe/src/tictactoe/Board.java
@@ -6,7 +6,7 @@ public class Board extends JPanel {
 	private final int SIZE = 190;
 
 	public Board(Square[][] squares) throws IllegalArgumentException {
-		if(squares == null) {
+		if(squares == null || squares.length != 3 || squares[0].length != 3) {
 			throw new IllegalArgumentException();
 		}
 
