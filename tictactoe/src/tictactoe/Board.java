7e85d959faf3a8b7bee96941168508f14bc0b232
DavidSjogren <sdavid2304@gmail.com>
2014-11-24 23:44:22 +0100

    REFACTOR

--- tictactoe/src/tictactoe/Board.java
+++ tictactoe/src/tictactoe/Board.java
@@ -6,8 +6,9 @@ public class Board extends JPanel {
 	private final int SIZE = 190;
 
 	public Board(Square[][] squares) throws IllegalArgumentException {
-
-		throw new IllegalArgumentException();
+		if(squares == null) {
+			throw new IllegalArgumentException();
+		}
 
 	}
 
