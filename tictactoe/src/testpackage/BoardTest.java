10e24754bac630a19faf36d63cc4c63c8cd7f819
DavidSjogren <sdavid2304@gmail.com>
2014-11-24 23:48:01 +0100

    RED

--- tictactoe/src/testpackage/BoardTest.java
+++ tictactoe/src/testpackage/BoardTest.java
@@ -1,10 +1,11 @@
 package testpackage;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 import tictactoe.Board;
+import tictactoe.Square;
 
 public class BoardTest {
 	private Board board;
@@ -13,6 +14,22 @@ public class BoardTest {
 	public void constructShouldThrowOnNullArg() {
 		try {
 			board = new Board(null);
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+
+	@Test
+	public void constructShouldThrowOnWrongSizeOfInputArray() {
+		Square[][] input = new Square[5][4];
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 4; j++) {
+				input[i][j] = new Square();
+			}
+		}
+		try {
+			board = new Board(input);
+			fail();
 		} catch (IllegalArgumentException e) {
 
 		}
