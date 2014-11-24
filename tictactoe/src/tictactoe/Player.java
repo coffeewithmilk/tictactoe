ae7e58733708a97faaf793a04e44cfaff8064d31
DavidSjogren <sdavid2304@gmail.com>
2014-11-24 19:33:11 +0100

    GREEN

--- tictactoe/src/tictactoe/Player.java
+++ tictactoe/src/tictactoe/Player.java
@@ -3,11 +3,10 @@ package tictactoe;
 public class Player {
 
 	public Player(String name) throws IllegalArgumentException {
-		if(name != null) {
-			
-		} else {
+		if(name == null || name.isEmpty()) {
 			throw new IllegalArgumentException();
-		}
+		} 
+		
 	}
 
 }
