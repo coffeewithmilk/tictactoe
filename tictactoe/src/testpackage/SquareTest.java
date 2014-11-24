220f2c00590da4a2d5defe3bf8fd615bbb4eaec4
DavidSjogren <sdavid2304@gmail.com>
2014-11-24 19:02:26 +0100

    test

---
+++ tictactoe/src/testpackage/SquareTest.java
@@ -0,0 +1,22 @@
+package testpackage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tictactoe.Square;
+
+public class SquareTest {
+	private Square square;
+	
+	
+	@Before
+	public void setUp() {
+		square = new Square();
+	}
+	
+	@Test
+	public void shouldThrow() {
+	
+	}
+	
+}
